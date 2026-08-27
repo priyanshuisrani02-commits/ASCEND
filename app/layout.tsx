855696cc388ece1339cccb2f8b7d5634796df467
priyanshuisrani02-commits <priyanshuisrani02@gmail.com>
2026-08-27 18:16:03 +0530

    feat: load chamber world aesthetics globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./cinematic.css";
+import "./worlds.css";
 import { CinematicTransition } from "@/components/CinematicTransition";
 
 const geistSans = Geist({
